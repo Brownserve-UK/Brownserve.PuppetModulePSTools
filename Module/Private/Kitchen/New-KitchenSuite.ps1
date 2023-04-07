938c94ff7604a3f05c2cb29bdf07e22cf904a984
Steve Brown <steve.brown+github@brownserve.co.uk>
2023-04-08 00:42:44 +0100

    add cmdlet for creating new-kitchensuite

---
+++ Module/Private/Kitchen/New-KitchenSuite.ps1
@@ -0,0 +1,92 @@
+function New-KitchenSuite
+{
+    [CmdletBinding()]
+    param
+    (
+        # The name of the suite
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]
+        $SuiteName,
+
+        # The name of the spec file associated with this suite
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string]
+        $SpecFileName,
+
+        # The path (relative to .kitchen.yml) to where the suite manifest will live
+        [Parameter(Mandatory = $false, Position = 2)]
+        [string]
+        $SpecFileRelativePath = 'spec/acceptance/',
+
+        # The execution command to run for this suite
+        [Parameter(Mandatory = $false, Position = 3)]
+        [string]
+        $SpecExecCommand = 'rspec -c -f d -I spec',
+
+        # The indentation to use for the returned YAML
+        [Parameter(Mandatory = $false)]
+        [int]
+        $Indentation = 2
+    )
+    
+    begin
+    {
+        
+    }
+    
+    process
+    {
+        if ($SpecFileName -notmatch '\.rb$')
+        {
+            $SpecFileName = $SpecFileName + '.rb'
+        }
+
+        $SuiteHash = @(
+                @{
+                    name     = $SuiteName
+                    verifier = @{
+                        command = ($SpecExecCommand + ' ' + $SpecFileRelativePath + $SpecFileName)
+                    }
+                }
+            )
+        try
+        {
+            $SuiteYaml = $SuiteHash | ConvertTo-Yaml -KeepArray -ErrorAction 'Stop'
+        }
+        catch
+        {
+            throw "Failed to converted suite hash to YAML.`n$($_.Exception.Message)"
+        }
+        # ConvertTo-Yaml is really designed to convert a complete YAML file and as such doesn't support indenting things
+        if ($Indentation -gt 0)
+        {
+            $SuiteYamlArray = $SuiteYaml -split "`n"
+            Clear-Variable 'SuiteYaml'
+            $Line = 0
+            $SuiteYamlArray | ForEach-Object {
+                $Line += 1
+                # Don't add a newline to the last line
+                if ($Line -eq $SuiteYamlArray.Count)
+                {
+                    $SuiteYaml += ' ' * $Indentation + $_ + "`r"
+                }
+                else
+                {
+                    $SuiteYaml += ' ' * $Indentation + $_ + "`n`r"
+                }
+            }
+        }
+    }
+    
+    end
+    {
+        if ($SuiteYaml)
+        {
+            return $SuiteYaml
+        }
+        else
+        {
+            return $null
+        }
+    }
+}
